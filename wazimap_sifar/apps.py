50f437402944121b928391d799c7876e4c46019f
lunga <stumenz.complex@gmail.com>
2018-11-14 10:24:53 +0200

    name app accordingly

--- wazimap_sifar/apps.py
+++ wazimap_sifar/apps.py
@@ -3,10 +3,5 @@ from django.conf import settings
 
 
 class WazimapConfig(AppConfig):
-    name = 'wazimap_za'
-    verbose_name = "Wazimap"
-
-    def ready(self):
-        if settings.WAZIMAP['default_profile'] == 'ecd':
-            from wazimap.views import HomepageView
-            HomepageView.template_name = 'homepage_ecd.html'
+    name = 'wazimap_sifar'
+    verbose_name = "Sifar"
