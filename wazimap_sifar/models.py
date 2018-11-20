89dfd3622243dac5c21603e7171019c5d17c5e6a
Eugene Prikazchikov <eprikazc@gmail.com>
2018-11-20 17:09:59 +0300

    Add PrivatePharmacy model

---
+++ wazimap_sifar/models.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+class PrivatePharmacy(models.Model):
+    province = models.CharField(blank=True, max_length=100)
+    district = models.CharField(blank=True, max_length=100)
+    sub_district = models.CharField(blank=True, max_length=100)
+    facility = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    address = models.CharField(max_length=200, blank=True)
+    organization_unit = models.CharField(max_length=200, blank=True)
+    organization_unit_type = models.CharField(max_length=200, blank=True)
+    geo_levels = ArrayField(
+        models.CharField(max_length=20), blank=True, null=True)
+
+    # settlement = models.CharField(max_length=100, blank=True)
+    # unit = models.CharField(max_length=50, blank=True)
+    # facility_code = models.CharField(max_length=20, unique=True)
+    # dataset = models.CharField(max_length=50)
+    # service = HStoreField()
+
+    class Meta:
+        db_table = 'private_pharmacies'
+        unique_together = ('facility', 'latitude', 'longitude', 'organization_unit_type')
+
+    def __str__(self):
+        return '%s, %s' % (self.facility, self.organization_unit_type)
