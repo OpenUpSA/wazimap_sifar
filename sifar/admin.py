e83974cd3dfe6fcd980b4415e4834d8c76911ea7
Eugene Prikazchikov <eprikazc@gmail.com>
2018-11-21 15:46:12 +0300

    Add CSV export

--- sifar/admin.py
+++ sifar/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from import_export.admin import ImportMixin
+from import_export.admin import ImportMixin, ImportExportModelAdmin
 
 from sifar.models import PrivatePharmacy
 from sifar.import_resource import PrivatePharmacyResource
 
 
-class PrivatePharmacyAdmin(ImportMixin, admin.ModelAdmin):
+class PrivatePharmacyAdmin(ImportExportModelAdmin):
     resource_class = PrivatePharmacyResource
     search_fields = ['facility']
 
