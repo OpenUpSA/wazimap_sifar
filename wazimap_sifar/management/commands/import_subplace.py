03cd81ca2499e6559f104a5f10cc40b5fcd17c0c
lunga <stumenz.complex@gmail.com>
2018-11-21 16:36:54 +0200

    insert subplaces to wazimap

---
+++ wazimap_sifar/management/commands/import_subplace.py
@@ -0,0 +1,35 @@
+import csv
+
+from django.core.management.base import BaseCommand, CommandError
+from wazimap.models import Geography
+"""
+Script to import subplaces to wazimap
+"""
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'filepath', action='store', help='File containing sub-places')
+
+    def handle(self, *args, **kwargs):
+        if not kwargs['filepath']:
+            raise CommandError("Subplace csv file needed")
+        with open(kwargs['filepath']) as csv_file:
+            try:
+                reader = csv.DictReader(csv_file)
+                for row in reader:
+                    obj, created = Geography.objects.update_or_create(
+                        {
+                            'version': 2011,
+                            'geo_code': row['SP_CODE'],
+                            'geo_level': 'subplace',
+                            'name': row['SP_NAME'],
+                            'parent_level': 'municipality',
+                            'parent_code': row['MN_MDB_C']
+                        },
+                        geo_code=row['SP_CODE'])
+                    self.stdout.write(self.style.SUCCESS('Sub-place added'))
+            except Exception as error:
+                self.stdout.write(self.style.ERROR('%s' % error))
+                raise
