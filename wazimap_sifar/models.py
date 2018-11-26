b02548a791a85e921a0cff0b69e4b3a8b0c33416
lunga <stumenz.complex@gmail.com>
2018-11-26 17:19:10 +0200

    add new types of data

--- wazimap_sifar/models.py
+++ wazimap_sifar/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-from django.contrib.postgres.fields import ArrayField
+from django.contrib.postgres.fields import ArrayField, HStoreField
 
 
 class PrivatePharmacy(models.Model):
@@ -27,8 +27,48 @@ class PrivatePharmacy(models.Model):
     class Meta:
         verbose_name_plural = 'private pharmacies'
         db_table = 'private_pharmacy'
-        unique_together = (
-            'facility', 'latitude', 'longitude', 'organization_unit_type')
+        unique_together = ('facility', 'latitude', 'longitude',
+                           'organization_unit_type')
 
     def __unicode__(self):
         return '%s, %s' % (self.facility, self.organization_unit_type)
+
+
+class HealthFacilities(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200, blank=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    settlement = models.CharField(max_length=100, blank=True)
+    unit = models.CharField(max_length=50, blank=True)
+    facility_code = models.CharField(max_length=20, unique=True)
+    geo_levels = ArrayField(
+        models.CharField(max_length=20), blank=True, null=True)
+    dataset = models.CharField(max_length=50)
+    service = HStoreField()
+
+    class Meta:
+        db_table = 'health_facilities'
+        unique_together = ('name', 'latitude', 'longitude', 'facility_code')
+
+    def __str__(self):
+        return self.name
+
+
+class ProfessionalService(models.Model):
+    title = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    service_code = models.CharField(max_length=20)
+    profession = models.CharField(max_length=100)
+    geo_levels = ArrayField(
+        models.CharField(max_length=20), blank=True, null=True)
+    details = HStoreField()
+
+    class Meta:
+        db_table = 'professional_service'
+
+    def __str__(self):
+        return '{} {} {}'.format(self.title, self.name, self.surname)
