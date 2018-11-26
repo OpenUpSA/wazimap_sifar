75d22db51c8583a62d30a8e32a9372da0f3183d3
lunga <stumenz.complex@gmail.com>
2018-11-26 17:19:34 +0200

    add models to admin panel

--- wazimap_sifar/admin.py
+++ wazimap_sifar/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 
-from wazimap_sifar.models import PrivatePharmacy
+from wazimap_sifar.models import PrivatePharmacy, HealthFacilities, ProfessionalService
 from wazimap_sifar.import_resource import PrivatePharmacyResource
 
 
@@ -10,4 +10,14 @@ class PrivatePharmacyAdmin(ImportExportModelAdmin):
     search_fields = ['facility']
 
 
+class HealthFacilitiesAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+class ProfessionalServiceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'name', 'surname', 'profession')
+
+
 admin.site.register(PrivatePharmacy, PrivatePharmacyAdmin)
+admin.site.register(HealthFacilities, HealthFacilitiesAdmin)
+admin.site.register(ProfessionalService, ProfessionalServiceAdmin)
