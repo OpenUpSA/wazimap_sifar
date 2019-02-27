9a68965f20f04618523f7432999f0f2b366cec6c
lunga <stumenz.complex@gmail.com>
2019-02-27 16:11:59 +0200

    update urls

--- wazimap_sifar/urls.py
+++ wazimap_sifar/urls.py
@@ -1,28 +1,30 @@
-from django.conf.urls import url, patterns, include
+from django.conf.urls import url, include
 
 from wazimap import urls
 from django.contrib import admin
 
 from .api import views as api
 
-urlpatterns = patterns('', url(r'^admin/', include(admin.site.urls)),
-                       url(r'^api/point/v1/sifar/private-pharmacies$',
-                           api.PrivatePharmacyView.as_view(),
-                           name='private_pharmacies'),
-                       url(r'^api/point/v1/sifar/health-services$',
-                           api.HealthFacilitiesView.as_view(),
-                           name='health_facilities'),
-                       url(r'^api/point/v1/sifar/community-parks$',
-                           api.CommunityParkView.as_view(),
-                           name='libraries'),
-                       url(r'^api/point/v1/sifar/district-parks$',
-                           api.DistrictParkView.as_view(),
-                           name='libraries'),
-                       url(r'^api/point/v1/sifar/libraries$',
-                           api.LibraryView.as_view(),
-                           name='libraries'),
-                       url(r'^api/point/v1/sifar/professional-services$',
-                           api.ProfessionalServiceView.as_view(),
-                           name='professional_service'))
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/point/v1/sifar/private-pharmacies$',
+        api.PrivatePharmacyView.as_view(),
+        name='private_pharmacies'),
+    url(r'^api/point/v1/sifar/health-services$',
+        api.HealthFacilitiesView.as_view(),
+        name='health_facilities'),
+    url(r'^api/point/v1/sifar/community-parks$',
+        api.CommunityParkView.as_view(),
+        name='libraries'),
+    url(r'^api/point/v1/sifar/district-parks$',
+        api.DistrictParkView.as_view(),
+        name='libraries'),
+    url(r'^api/point/v1/sifar/libraries$',
+        api.LibraryView.as_view(),
+        name='libraries'),
+    url(r'^api/point/v1/sifar/professional-services$',
+        api.ProfessionalServiceView.as_view(),
+        name='professional_service')
+]
 
 urlpatterns += urls.urlpatterns
