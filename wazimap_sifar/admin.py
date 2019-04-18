ada41edc2bdfeb2f6a75c978b066f3d86e8b6b58
lunga <stumenz.complex@gmail.com>
2019-04-18 07:20:40 +0200

    add fields to admin panel

--- wazimap_sifar/admin.py
+++ wazimap_sifar/admin.py
@@ -1,40 +1,45 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 
-from wazimap_sifar.models import (
-    CommunityPark, DistrictPark, HealthFacilities,
-    Library, PrivatePharmacy, ProfessionalService)
+from wazimap_sifar.models import (CommunityPark, DistrictPark,
+                                  HealthFacilities, Library, PrivatePharmacy,
+                                  ProfessionalService)
 from wazimap_sifar.import_resource import (
-    CommunityParkResource, DistrictParkResource,
-    LibraryResource, PrivatePharmacyResource)
+    CommunityParkResource, DistrictParkResource, LibraryResource,
+    PrivatePharmacyResource)
 
 
 class PrivatePharmacyAdmin(ImportExportModelAdmin):
     resource_class = PrivatePharmacyResource
-    search_fields = ['facility']
+    search_fields = ['name']
+    list_display = ('name', 'organization_unit', 'organization_unit_type',
+                    'geo_levels')
 
 
 class CommunityParkAdmin(ImportExportModelAdmin):
     resource_class = CommunityParkResource
     search_fields = ['name', 'address', 'suburb']
+    list_display = ('name', 'suburb', 'geo_levels')
 
 
 class DistrictParkAdmin(ImportExportModelAdmin):
     resource_class = DistrictParkResource
     search_fields = ['name', 'address', 'suburb']
+    list_display = ('name', 'suburb', 'geo_levels')
 
 
 class LibraryAdmin(ImportExportModelAdmin):
     resource_class = LibraryResource
     search_fields = ['name', 'library_type']
+    list_display = ('name', 'members', 'library_type', 'geo_levels')
 
 
 class HealthFacilitiesAdmin(admin.ModelAdmin):
-    list_display = ('name', )
+    list_display = ('name', 'settlement', 'unit', 'geo_levels')
 
 
 class ProfessionalServiceAdmin(admin.ModelAdmin):
-    list_display = ('title', 'name', 'surname', 'profession')
+    list_display = ('title', 'name', 'surname', 'profession', 'geo_levels')
 
 
 admin.site.register(PrivatePharmacy, PrivatePharmacyAdmin)
