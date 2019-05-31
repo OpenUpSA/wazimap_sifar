64367c6310662f6c9ef005c3531fb93863b600cb
lunga <stumenz.complex@gmail.com>
2019-05-31 14:51:55 +0200

    update models

--- wazimap_sifar/admin.py
+++ wazimap_sifar/admin.py
@@ -1,50 +1,50 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 
-from wazimap_sifar.models import (CommunityPark, DistrictPark,
-                                  HealthFacilities, Library, PrivatePharmacy,
-                                  ProfessionalService)
-from wazimap_sifar.import_resource import (
-    CommunityParkResource, DistrictParkResource, LibraryResource,
-    PrivatePharmacyResource)
+from wazimap_sifar.models import Dataset, DatasetCategory, Contributer
+from wazimap_sifar.resource import DatasetResource
+from wazimap_sifar.forms import CustomImportForm, CustomConfirmImportForm
 
 
-class PrivatePharmacyAdmin(ImportExportModelAdmin):
-    resource_class = PrivatePharmacyResource
-    search_fields = ['name']
-    list_display = ('name', 'organization_unit', 'organization_unit_type',
-                    'geo_levels')
+class DatasetAdmin(ImportExportModelAdmin):
+    resource_class = DatasetResource
+    list_display = ('name', 'address', 'latitude', 'longitude', 'email',
+                    'website', 'phone_number', 'contributer')
+    search_fields = ('name', )
+    list_filter = ('contributer__subcategory', )
+
+    def get_import_form(self):
+        return CustomImportForm
+
+    def get_confirm_input_form():
+        return CustomConfirmImportForm
+
+    def get_resource_kwargs(self, request, *args, **kwargs):
+        rk = super(DatasetAdmin, self).get_resource_kwargs(
+            request, *args, **kwargs)
+        rk['contributer'] = None
+        if request.POST:
+            contributer = request.POST.get('contributer', None)
+            if contributer:
+                request.session['contributer'] = contributer
+            else:
+                try:
+                    # If we don't have it from a form field, we should find it in the session.
+                    contributer = request.session['contributer']
+                except KeyError as e:
+                    raise Exception("Context failure on row import, " + e)
+            rk['contributer'] = contributer
+        return rk
 
 
-class CommunityParkAdmin(ImportExportModelAdmin):
-    resource_class = CommunityParkResource
-    search_fields = ['name', 'address', 'suburb']
-    list_display = ('name', 'suburb', 'geo_levels')
+class ContributerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'category', 'subcategory', 'approved')
 
 
-class DistrictParkAdmin(ImportExportModelAdmin):
-    resource_class = DistrictParkResource
-    search_fields = ['name', 'address', 'suburb']
-    list_display = ('name', 'suburb', 'geo_levels')
-
-
-class LibraryAdmin(ImportExportModelAdmin):
-    resource_class = LibraryResource
-    search_fields = ['name', 'library_type']
-    list_display = ('name', 'members', 'library_type', 'geo_levels')
-
-
-class HealthFacilitiesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'settlement', 'unit', 'geo_levels')
-
-
-class ProfessionalServiceAdmin(admin.ModelAdmin):
-    list_display = ('title', 'name', 'surname', 'profession', 'geo_levels')
-
-
-admin.site.register(PrivatePharmacy, PrivatePharmacyAdmin)
-admin.site.register(HealthFacilities, HealthFacilitiesAdmin)
-admin.site.register(ProfessionalService, ProfessionalServiceAdmin)
-admin.site.register(Library, LibraryAdmin)
-admin.site.register(CommunityPark, CommunityParkAdmin)
-admin.site.register(DistrictPark, DistrictParkAdmin)
+admin.site.register(Dataset, DatasetAdmin)
+admin.site.register(DatasetCategory)
+admin.site.register(Contributer, ContributerAdmin)
--- wazimap_sifar/models.py
+++ wazimap_sifar/models.py
@@ -1,112 +1,50 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-from django.contrib.postgres.fields import ArrayField, HStoreField
+
+class DatasetCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'sifar_dataset_category'
+        verbose_name = 'Dataset Category'
+
+    def __str__(self):
+        return self.name
 
 
-class GeoItem(models.Model):
+class Contributer(models.Model):
+    user = models.ForeignKey(User)
+    category = models.ForeignKey(
+        DatasetCategory, on_delete=models.CASCADE, null=True)
+    subcategory = models.CharField(max_length=255)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'sifar_contributers'
+
+    def __str__(self):
+        return self.subcategory
+
+
+class Dataset(models.Model):
+    contributer = models.ForeignKey(
+        Contributer, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=255)
     latitude = models.FloatField()
     longitude = models.FloatField()
-    geo_levels = ArrayField(
-        models.CharField(max_length=20), blank=True, null=True)
+    address = models.CharField(max_length=255, null=True)
+    type = models.CharField(max_length=255, null=True)
+    email = models.EmailField(null=True)
+    phone_number = models.CharField(max_length=10, null=True)
+    website = models.URLField(null=True)
 
     class Meta:
-        abstract = True
-
-
-class PrivatePharmacy(GeoItem):
-    province = models.CharField(blank=True, max_length=100)
-    district = models.CharField(blank=True, max_length=100)
-    sub_district = models.CharField(blank=True, max_length=100)
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=200, blank=True)
-    organization_unit = models.CharField(max_length=200, blank=True)
-    organization_unit_type = models.CharField(max_length=200, blank=True)
-
-    # settlement = models.CharField(max_length=100, blank=True)
-    # unit = models.CharField(max_length=50, blank=True)
-    # facility_code = models.CharField(max_length=20, unique=True)
-    # dataset = models.CharField(max_length=50)
-    # service = HStoreField()
-
-    class Meta:
-        verbose_name_plural = 'private pharmacies'
-        db_table = 'private_pharmacy'
-        unique_together = ('name', 'latitude', 'longitude',
-                           'organization_unit_type')
-
-    def __unicode__(self):
-        return '%s, %s' % (self.facility, self.organization_unit_type)
-
-
-class HealthFacilities(GeoItem):
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=200, blank=True)
-    settlement = models.CharField(max_length=100, blank=True)
-    unit = models.CharField(max_length=50, blank=True)
-    facility_code = models.CharField(max_length=20, unique=True)
-    dataset = models.CharField(max_length=50)
-    service = HStoreField()
-
-    class Meta:
-        db_table = 'health_facilities'
-        unique_together = ('name', 'latitude', 'longitude', 'facility_code')
+        db_table = "sifar_dataset"
+        unique_together = ('address', 'name', 'latitude', 'longitude',
+                           'contributer')
 
     def __str__(self):
-        return self.name
-
-
-class Library(GeoItem):
-    name = models.CharField(max_length=100, blank=True)
-    members = models.IntegerField(blank=True, null=True)
-    library_type = models.CharField(max_length=100, blank=True)
-
-    class Meta:
-        verbose_name_plural = 'libraries'
-        db_table = 'library'
-        unique_together = ('name', 'latitude', 'longitude', 'library_type')
-
-    def __str__(self):
-        return self.name
-
-
-class CommunityPark(GeoItem):
-    name = models.CharField(max_length=100, blank=True)
-    address = models.CharField(max_length=100, blank=True)
-    suburb = models.CharField(max_length=100, blank=True)
-
-    class Meta:
-        db_table = 'community_parks'
-        unique_together = ('name', 'suburb')
-
-    def __str__(self):
-        return self.name
-
-
-class DistrictPark(GeoItem):
-    name = models.CharField(max_length=100, blank=True)
-    address = models.CharField(max_length=100, blank=True)
-    suburb = models.CharField(max_length=100, blank=True)
-
-    class Meta:
-        db_table = 'district_parks'
-        unique_together = ('name', 'suburb')
-
-    def __str__(self):
-        return self.name
-
-
-class ProfessionalService(GeoItem):
-    title = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    service_code = models.CharField(max_length=20)
-    profession = models.CharField(max_length=100)
-    details = HStoreField()
-
-    class Meta:
-        db_table = 'professional_service'
-
-    def __str__(self):
-        return '{} {} {}'.format(self.title, self.name, self.surname)
+        return self.contributer.subcategory
