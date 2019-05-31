f6dee980bd3064f01d8c9c3a07c85414d0f45b30
lunga <stumenz.complex@gmail.com>
2019-05-31 14:51:27 +0200

    serialize model data

---
+++ wazimap_sifar/utils.py
@@ -0,0 +1,20 @@
+from geojson import FeatureCollection, Feature, Point
+
+
+def to_geojson(model):
+    """
+    Convert django model to geojson
+    """
+    feature_collection = []
+    for record in model.objects.all():
+        properies = {
+            'name': record.name,
+            'address': record.address,
+            'email': record.email,
+            'website': record.website,
+            'phone_number': record.phone_number,
+        }
+        my_point = Point((record.longitude, record.latitude))
+        my_feature = Feature(geometry=my_point, properties=properies)
+        feature_collection.append(my_feature)
+    return FeatureCollection(feature_collection)
