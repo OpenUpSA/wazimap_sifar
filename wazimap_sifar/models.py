b05f1ffc11fae432dc50e526b7bf363745087236
lunga <stumenz.complex@gmail.com>
2019-08-16 10:50:37 +0200

    add plural name

--- wazimap_sifar/models.py
+++ wazimap_sifar/models.py
@@ -8,8 +8,8 @@ class DatasetCategory(models.Model):
     name = models.CharField(max_length=255)
 
     class Meta:
-        db_table = 'sifar_dataset_category'
-        verbose_name = 'Dataset Category'
+        db_table = "sifar_dataset_category"
+        verbose_name_plural = "Dataset Categories"
 
     def __str__(self):
         return self.name
@@ -17,21 +17,19 @@ class DatasetCategory(models.Model):
 
 class Contributer(models.Model):
     user = models.ForeignKey(User)
-    category = models.ForeignKey(
-        DatasetCategory, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(DatasetCategory, on_delete=models.CASCADE, null=True)
     subcategory = models.CharField(max_length=255)
     approved = models.BooleanField(default=False)
 
     class Meta:
-        db_table = 'sifar_contributers'
+        db_table = "sifar_contributers"
 
     def __str__(self):
         return self.subcategory
 
 
 class Dataset(models.Model):
-    contributer = models.ForeignKey(
-        Contributer, on_delete=models.CASCADE, null=True)
+    contributer = models.ForeignKey(Contributer, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=255)
     latitude = models.FloatField()
     longitude = models.FloatField()
@@ -43,8 +41,7 @@ class Dataset(models.Model):
 
     class Meta:
         db_table = "sifar_dataset"
-        unique_together = ('address', 'name', 'latitude', 'longitude',
-                           'contributer')
+        unique_together = ("address", "name", "latitude", "longitude", "contributer")
 
     def __str__(self):
         return self.contributer.subcategory
