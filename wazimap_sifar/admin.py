b50cbe756e0d70a1909d97a5d0310c452a04b37a
lunga <stumenz.complex@gmail.com>
2019-09-02 16:35:00 +0200

    hide type field

--- wazimap_sifar/admin.py
+++ wazimap_sifar/admin.py
@@ -1,6 +1,7 @@
 import sys
+
 reload(sys)
-sys.setdefaultencoding('utf8')
+sys.setdefaultencoding("utf8")
 
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
@@ -12,10 +13,19 @@ from wazimap_sifar.forms import CustomImportForm, CustomConfirmImportForm
 
 class DatasetAdmin(ImportExportModelAdmin):
     resource_class = DatasetResource
-    list_display = ('name', 'address', 'latitude', 'longitude', 'email',
-                    'website', 'phone_number', 'contributer')
-    search_fields = ('name', )
-    list_filter = ('contributer__subcategory', )
+    list_display = (
+        "name",
+        "address",
+        "latitude",
+        "longitude",
+        "email",
+        "website",
+        "phone_number",
+        "contributer",
+    )
+    exclude = ("type",)
+    search_fields = ("name",)
+    list_filter = ("contributer__subcategory",)
 
     def get_import_form(self):
         return CustomImportForm
@@ -24,25 +34,24 @@ class DatasetAdmin(ImportExportModelAdmin):
         return CustomConfirmImportForm
 
     def get_resource_kwargs(self, request, *args, **kwargs):
-        rk = super(DatasetAdmin, self).get_resource_kwargs(
-            request, *args, **kwargs)
-        rk['contributer'] = None
+        rk = super(DatasetAdmin, self).get_resource_kwargs(request, *args, **kwargs)
+        rk["contributer"] = None
         if request.POST:
-            contributer = request.POST.get('contributer', None)
+            contributer = request.POST.get("contributer", None)
             if contributer:
-                request.session['contributer'] = contributer
+                request.session["contributer"] = contributer
             else:
                 try:
                     # If we don't have it from a form field, we should find it in the session.
-                    contributer = request.session['contributer']
+                    contributer = request.session["contributer"]
                 except KeyError as e:
                     raise Exception("Context failure on row import, " + e)
-            rk['contributer'] = contributer
+            rk["contributer"] = contributer
         return rk
 
 
 class ContributerAdmin(admin.ModelAdmin):
-    list_display = ('user', 'category', 'subcategory', 'approved')
+    list_display = ("user", "category", "subcategory", "approved")
 
 
 admin.site.register(Dataset, DatasetAdmin)
