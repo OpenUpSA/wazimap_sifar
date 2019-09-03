b2fa479684ed351a98e70b9d727cf610dc833d68
lunga <stumenz.complex@gmail.com>
2019-09-03 16:15:37 +0200

    remove

--- wazimap_sifar/settings.py
+++ wazimap_sifar/settings.py
@@ -56,9 +56,6 @@ MIDDLEWARE_CLASSES = (
 ) + MIDDLEWARE_CLASSES
 
 
-if DEBUG:
-    GEOS_LIBRARY_PATH = "/usr/lib/libgeos_c.so"
-
 DATABASE_URL = os.environ.get(
     "DATABASE_URL", "postgresql://wazimap_sifar:wazimap_sifar@development/wazimap_sifar"
 )
