3d2c4ae0bf42401a32385b4192fb4388be9f6320
ebsuku <stumenz.complex@gmail.com>
2020-04-03 09:16:39 +0200

    fix models for rest api

--- wazimap_sifar/models.py
+++ wazimap_sifar/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 
 class DatasetCategory(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     class Meta:
         db_table = "sifar_dataset_category"
@@ -23,6 +23,7 @@ class Contributor(models.Model):
 
     class Meta:
         db_table = "sifar_contributers"
+        unique_together = ("category", "subcategory")
 
     def __str__(self):
         return self.subcategory
@@ -30,20 +31,29 @@ class Contributor(models.Model):
 
 class Dataset(models.Model):
     contributer = models.ForeignKey(
-        Contributor, on_delete=models.CASCADE, null=True, verbose_name="Source"
+        Contributor, on_delete=models.CASCADE, null=True, verbose_name="Contributor"
     )
     name = models.CharField(max_length=255)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
     address = models.CharField(max_length=255, null=True, blank=True)
     type = models.CharField(max_length=255, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
-    phone_number = models.CharField(max_length=10, null=True, blank=True)
-    website = models.URLField(null=True, blank=True)
+    phone_number = models.CharField(max_length=50, null=True, blank=True)
+    website = models.CharField(max_length=255, null=True, blank=True)
 
     class Meta:
         db_table = "sifar_dataset"
-        unique_together = ("address", "name", "latitude", "longitude", "contributer")
+        unique_together = (
+            "address",
+            "name",
+            "latitude",
+            "longitude",
+            "contributer",
+            "email",
+            "website",
+            "phone_number",
+        )
 
     def __str__(self):
         return self.contributer.subcategory
