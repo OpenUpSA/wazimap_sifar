d4653dda5aa46667a6b42eda06fa6263cd0bebf7
ebsuku <stumenz.complex@gmail.com>
2020-05-05 12:11:07 +0200

    catch where there are no latitude and longitudes

--- wazimap_sifar/utils.py
+++ wazimap_sifar/utils.py
@@ -7,14 +7,17 @@ def to_geojson(model, contrib_id):
     """
     feature_collection = []
     for record in model.objects.filter(contributer_id=contrib_id):
-        properies = {
-            'name': record.name,
-            'address': record.address,
-            'email': record.email,
-            'website': record.website,
-            'phone_number': record.phone_number,
-        }
-        my_point = Point((record.longitude, record.latitude))
-        my_feature = Feature(geometry=my_point, properties=properies)
-        feature_collection.append(my_feature)
+        try:
+            properies = {
+                "name": record.name,
+                "address": record.address,
+                "email": record.email,
+                "website": record.website,
+                "phone_number": record.phone_number,
+            }
+            my_point = Point((record.longitude, record.latitude))
+            my_feature = Feature(geometry=my_point, properties=properies)
+            feature_collection.append(my_feature)
+        except ValueError:
+            pass
     return FeatureCollection(feature_collection)
